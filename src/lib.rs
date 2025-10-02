5c23d2dd21c3f30faf7d71707bbc2c76f2daf2f6
Marvel Michael <marvelmichael67@gmail.com>
2025-10-02 10:09:16 +0700

    added keygen

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use solana_sdk;
+    use solana_sdk::signature::{Keypair, Signer};
 
     #[test]
-    fn keygen() {}
+    fn keygen() {
+        // Create a new keypair
+        let kp = Keypair::new();
+        println!("Solana wallet has been generate: {}\n", kp.pubkey());
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
 
     #[test]
     fn claim_airdrop() {}
